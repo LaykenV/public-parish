4f6fa123bdb2a1c06329acdcab4e283635cf0063
Layken Varholdt <96850110+LaykenV@users.noreply.github.com>
2026-09-07 13:50:18 -0500

    style: separate story route imports

--- src/routes/stories.$storySlug.tsx
+++ src/routes/stories.$storySlug.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { ResidentShell } from '../features/resident-blueprint/resident-shell'
 import { StoryPage } from '../features/stories/story-page'
+
 export const Route = createFileRoute('/stories/$storySlug')({ component: StoryRoute })
 function StoryRoute() {
   const { storySlug } = Route.useParams()
